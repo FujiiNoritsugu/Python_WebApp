032579f055a35ec04479b5b4835b41d2fb4d6557
FujiiNoritsugu <quercus491@gmail.com>
2022-06-23 08:00:36 +0900

    演習の続き

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from test_model import Person
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exercise_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -28,17 +33,6 @@ def show_html():
     return render_template('./test_html.html')
 
 
-@app.route('/show_exercise')
-def show_exercise():
-    return render_template('./exercise.html')
-
-
-@app.route('/exercise')
-def show_answer():
-    my_name = request.args.get("my_name")
-    return render_template('./answer.html', name=my_name)
-
-
 @app.route('/try_rest', methods=['POST'])
 def try_rest():
     # リクエストデータをJSONとして受け取る
@@ -49,3 +43,32 @@ def try_rest():
     print(name)
     response_json = {"response_json": request_json}
     return jsonify(response_json)
+
+
+@app.route('/person_search')
+def person_search():
+    return render_template('./person_search.html')
+
+
+@app.route('/person_result')
+def person_result():
+    search_size = request.args.get("search_size")
+    persons = db.session.query(Person).filter(Person.size > search_size)
+    return render_template('./person_result.html', persons=persons, search_size=search_size)
+
+# ここから演習の回答例
+from exercise_model import Human
+from sqlalchemy import or_
+
+@app.route('/human_search')
+def human_search():
+    return render_template('./human_search.html')
+
+@app.route('/human_result')
+def human_result():
+    search_height = request.args.get('search_height')
+    search_weight = request.args.get('search_weight')
+    humans = db.session.query(Human).filter(or_(Human.height>=search_height, Human.weight>=search_weight))
+    return render_template('./human_result.html', humans=humans, search_height=search_height, search_weight=search_weight)
+
+
