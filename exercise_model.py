e68a35ed147fd3ba721888ca07a6f25913a8ca03
FujiiNoritsugu <quercus491@gmail.com>
2022-06-23 06:37:15 +0900

    演習続き分

---
+++ exercise_model.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from sqlalchemy import Column, Float, Integer, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Human(Base):
+    __tablename__ = 'human'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    height = Column(Float)
+    weight = Column(Float)
